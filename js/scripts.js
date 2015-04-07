60eb7a0568a87d4db0f96ef7bf2f122193db5d7a
Reid Baldwin & James Nielson <student@epicodus.com>
2015-04-07 11:04:07 -0700

    add multi-word title functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
 var titleCase  = function(input) {
-    return input.charAt(0).toUpperCase() + input.slice(1);
+    var word_array = input.split(' ');
+    var output= [];
+    word_array.forEach(function(word) {
+        output.push(word.charAt(0).toUpperCase() + word.slice(1));
+    });
+    var final_output = output.join(' ');
+    return final_output;
 };
+
+
+// for(var i=0; i < input.length; i++){
+//     var word = word_array[i];
+//     word.charAt[0].toUpperCase() + word.slice(1);
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,11 @@ describe("titleCase", function() {
   it("capitalizes the first letter of a word", function() {
     expect(titleCase("cat")).to.equal("Cat");
   });
+
+
+  it("capitalizes the first letter of every word in a multi-word title", function() {
+          expect(titleCase("tale of two cities")).to.equal("Tale Of Two Cities");
+    });
+
+
 });
